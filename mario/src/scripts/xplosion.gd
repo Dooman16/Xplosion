f69787445a2279a1a43e5108ada8694b08548fea
Dooman16 <benjaminrodriguezsoderlund@gmail.com>
2025-06-13 00:37:22 -0300

    lanza basica terminada, nombre posible de juego, xplosion

--- mario/src/scripts/xplosion.gd
+++ mario/src/scripts/xplosion.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export var projectile : PackedScene
-@onready var melee_range := $melee_range
+@onready var body := $hitbox
 
 const MAX_RETRACTION_DISTANCE := 20
 const MAX_ATTACK_RANGE := 45
@@ -10,102 +10,104 @@ const HOOK_CHARGING_TIME := 3
 const MAX_DISTANCE_PER_SPEED := 1
 const ATTACK_COOLDOWN := 1.2
 const MELEE_ATTACK_CHARGING_TIME := 0.1
+const DIRECTION := Vector2(1, 0)
+const STARTING_POS = Vector2(22.5,0)
 
-var direction := Vector2(1,0)
 var charge := 0.0
-var starting_pos : Vector2
 var speed := 0.0
-var distance_coverd := 0.0
 var meleeing := false
 var charged := false
 var thrown := false
-var last_position := Vector2.ZERO
+
+func _ready():
+	# Conectar señales del área de colisión
+	body.connect("area_entered", Callable(self, "_on_area_entered"))
+	body.connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _physics_process(delta: float) -> void:
 	if speed == 0:
-		if Input.is_action_just_pressed("left_attack"):
-			starting_pos = position
 		if Input.is_action_pressed("left_attack"):
-			if melee_range.is_colliding() and charge == 0:
-				meleeing = true
-			else:
-				charge_range_attack(delta)
+			charge_range_attack(delta)
 		elif Input.is_action_just_released("left_attack"):
 			release_range_attack()
 		if Input.is_action_pressed("right_attack"):
-			charge_hook()
-		elif Input.is_action_just_released("right_attack"):
-			release_hook()
+			meleeing = true
 	else:
 		move(delta)
+
 	if meleeing:
 		melee_attack(delta)
-	last_position = position
 
 func charge_range_attack(delta: float):
-	direction = get_mouse_vectorial_difference().normalized()
-	if MAX_RETRACTION_DISTANCE >= charge:
-		charge += delta*MAX_RETRACTION_DISTANCE / CHARGING_TIME
-		position = starting_pos - charge*direction
+	if charge <= MAX_RETRACTION_DISTANCE:
+		charge += delta * MAX_RETRACTION_DISTANCE / CHARGING_TIME
+		body.position = STARTING_POS - charge*DIRECTION
 	else:
 		charge = MAX_RETRACTION_DISTANCE
-		position = starting_pos - (charge - (randf()*2 -1))*direction
-	rotation = get_xplosion_rotation()
+		body.position = STARTING_POS - (charge - (randf() * 2 - 1))*DIRECTION
+
+	global_rotation = get_xplosion_rotation()
+
 func release_range_attack():
-	speed = charge*30
+	speed = charge * 30
 	charge = 0.0
-	
+
 func melee_attack(delta: float):
 	const MELEE_SPEED = MAX_RETRACTION_DISTANCE / MELEE_ATTACK_CHARGING_TIME
-	direction = get_mouse_vectorial_difference().normalized()
-	if MAX_RETRACTION_DISTANCE/2 >= position.length() and not charged:
-		position -= direction*delta*MELEE_SPEED
-	elif MAX_ATTACK_RANGE >= position.length() and not thrown:
+	global_rotation = get_xplosion_rotation()
+
+	var distance_to_start = (body.position - STARTING_POS).length()
+
+	if distance_to_start <= MAX_RETRACTION_DISTANCE / 2 and not charged:
+		body.position -= delta * MELEE_SPEED*DIRECTION
+	elif distance_to_start <= MAX_ATTACK_RANGE and not thrown:
 		charged = true
-		position += direction*delta*MELEE_SPEED
-	elif position.length() > 1 :
-		position -= direction*delta*MELEE_SPEED
+		body.position += delta * MELEE_SPEED*DIRECTION
+	elif distance_to_start > 1:
+		body.position -= delta * MELEE_SPEED*DIRECTION
 		thrown = true
 	else:
-		position = starting_pos
+		body.position = STARTING_POS
 		charged = false
 		thrown = false
 		meleeing = false
-	rotation = get_xplosion_rotation()
-func charge_hook():
-	pass
-func release_hook():
-	pass
-	
-func get_xplosion_rotation():
-	var vectorial_diference = get_mouse_vectorial_difference()
-	return atan2(vectorial_diference.y,vectorial_diference.x)
-	
+
+func move(delta: float):
+	body.position += DIRECTION * speed * delta
+	var distance_covered = (body.position - STARTING_POS).length()
+
+	if speed > 0:
+		if distance_covered >= MAX_DISTANCE_PER_SPEED * speed:
+			return_lance()
+	elif speed < 0:
+		if distance_covered < 1:
+			body.position = STARTING_POS
+			speed = 0
+			body.disabled = false
+
+func return_lance():
+	print("hello")
+	body.disabled = true
+	var distance_covered = (body.position - STARTING_POS).length()
+	speed = -distance_covered / ATTACK_COOLDOWN
+
+func get_xplosion_rotation() -> float:
+	var vectorial_difference = get_mouse_vectorial_difference()
+	return atan2(vectorial_difference.y, vectorial_difference.x)
+
 func get_mouse_vectorial_difference() -> Vector2:
-	return get_global_mouse_position()-global_position
+	return get_global_mouse_position() - global_position
 
 func _on_area_entered(area: Area2D) -> void:
 	if speed > 0:
 		return_lance()
-		
-func _on_body_entered(body: Node2D) -> void:
+
+func _on_body_entered(body_node: Node2D) -> void:
 	if speed > 0:
 		return_lance()
 
-func move(delta: float):
-	position += direction*speed*delta
-	if speed > 0:
-		distance_coverd = position.length()
-		if distance_coverd >= MAX_DISTANCE_PER_SPEED*speed:
-			return_lance()
-	elif speed < 0:
-		if position.length() > last_position.length():
-			position = Vector2.ZERO
-			speed = 0
-			$CollisionShape2D.disabled = false
+func charge_hook():
+	pass
 
-func return_lance():
-	print("hello")
-	$CollisionShape2D.disabled = true
-	speed = (-1)*(distance_coverd / ATTACK_COOLDOWN)
-	#position = Vector2.ZERO
+func release_hook():
+	pass
